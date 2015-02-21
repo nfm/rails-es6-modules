b73240db8a5f565bd021db1e213ddae95faf2491
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2015-02-22 09:05:37 +1100

    Configure sprockets to use system.js for ES6 modules

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,9 @@ module Es6
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    Rails.application.config.assets.configure do |env|
+      env.register_transformer 'text/ecmascript-6', 'application/javascript', Sprockets::ES6.new('modules' => 'system', 'moduleIds' => true)
+    end
   end
 end
